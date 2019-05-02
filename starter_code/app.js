9858a745ff074a293b52415fb25677c6b1b71a01
Monika Woznowska <monike@poczta.onet.eu>
2019-05-02 10:59:56 +0200

    routes working

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,36 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', {title:'Home'});
 });
 
+app.get("/beers", (req, res) => {
+  var beers = punkAPI.getBeers().then(beers => {
+    console.log(JSON.stringify(beers, null, 2))
+    res.render("beers", {beers: beers, title: "Beers"});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  console.log(beers);
+  
+})
 
+app.get("/random-beer", (req, res) => {
 
-app.listen(3000);
+  punkAPI.getRandom().then(beers => {
+    
+    res.render("beers", {beers: beers, title: "Random Beer"});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+})
+
+app.listen(3000, ()=> {
+  console.log("listening on port 3000")
+});
